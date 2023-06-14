3ef282c247cb69a9cbe44b5409aa63faede96c85
Akash Gaur <134853842+akgaur12@users.noreply.github.com>
2023-06-14 10:08:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,29 @@ en = Entry(
     textvariable=var,
 )
 en.place(x=15, y=10, height=55, width=333, )
+
+#placing buttons
+b = [Button()]*16
+data = ('1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', 'x', 'C', '0', '=', '/')
+k = 0
+x = 15
+y = 75
+for i in range(4):
+    for j in range(4):
+        b[k] = Button(
+            root,
+            text=data[k],
+            font=('', 20, 'bold'),
+            height=1,
+            width = 4,
+            bg='light gray',
+            fg='black',
+            activebackground='light green',
+        )
+        b[k].place(x=x, y=y)
+        k+=1
+        x += 85
+    x = 15
+    y += 65
+    
+    
